a04a26a14b0019fecc372469de2fc1bf57d4e653
Frosty1604 <10773180+Frosty1604@users.noreply.github.com>
2023-01-19 23:20:32 +0100

    add input field to import db json

--- apps/time-tracker/src/app/routes/export/export.component.ts
+++ apps/time-tracker/src/app/routes/export/export.component.ts
@@ -9,15 +9,17 @@ import { Subject, switchMap, takeUntil } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { WorkTimeService } from '../../core/services/work-time.service';
 import exportFromJSON from 'export-from-json';
+import { MatInputModule } from '@angular/material/input';
+import { WorkTimePartial } from '../../core/entities/work-time.entity';
 
 @Component({
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatInputModule],
   templateUrl: './export.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExportComponent implements OnInit, OnDestroy {
-  private readonly workTimeData = inject(WorkTimeService).find();
+  private readonly workTimeService = inject(WorkTimeService);
   private readonly exportSubject = new Subject<void>();
   private readonly onDestroySubject = new Subject<void>();
 
@@ -25,7 +27,7 @@ export class ExportComponent implements OnInit, OnDestroy {
     this.exportSubject
       .asObservable()
       .pipe(
-        switchMap(() => this.workTimeData),
+        switchMap(() => this.workTimeService.find()),
         takeUntil(this.onDestroySubject)
       )
       .subscribe((data) => {
@@ -45,5 +47,22 @@ export class ExportComponent implements OnInit, OnDestroy {
 
   exportData() {
     this.exportSubject.next();
+  }
+
+  async fileChange($event: Event) {
+    const file = ($event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const json = await file.text();
+      try {
+        const dbData: WorkTimePartial[] = JSON.parse(json);
+        for (const workTime of dbData) {
+          delete workTime.id;
+          workTime.date = new Date(workTime.date);
+          await this.workTimeService.insert(workTime);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
   }
 }
